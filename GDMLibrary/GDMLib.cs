e0ff87de740a90ee6803cada87901d5f6cf7b363
Jarosław Komar <jaszczur666@o2.pl>
2023-03-22 05:03:25 +0100

    Added code of two minor libraries needed for the experimental loop

---
+++ GDMLibrary/GDMLib.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics;
+using System.IO.Ports;
+using System.Threading;
+using System;
+
+namespace GDMLib
+    {
+        public class GDM
+        {
+            private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            private SerialPort MotorPort;
+        public int stepNum;
+        private double curWavenumber=0;
+        private const int single_step_duration = 100;
+        private bool bInitialized;
+        private int stepNumber = 0;
+        public GDM()
+        {
+            MotorPort = new SerialPort
+            {
+                BaudRate = 9600,
+                Parity = Parity.None,
+                DataBits = 8,
+                StopBits = StopBits.One
+            };
+        }
+        public void SetCurrentWaveNumber(double wn) { curWavenumber = wn; }
+        public void Parse(string command)
+        {
+            string[] comarg;
+            if (command.Contains(" ")) comarg = command.Split(' ');
+            else
+            {
+                comarg = new string[1];
+                comarg[0] = command;
+            }
+            switch (comarg[0]) {
+                case "step":
+                    {
+                        int.TryParse(comarg[1], out int spp);
+                        Step(spp);
+                        break;
+                    }
+            }
+        }
+        private void Step(int numsteps)
+        {
+            if (bInitialized)
+            {
+                
+                for (int i = 0; i < numsteps; i++)
+                {
+                    Stopwatch stopWatch = new Stopwatch();
+                    stopWatch.Start();
+                    string pom = "0" + ((stepNumber % 4)+1).ToString();
+                    MotorPort.Write(pom);
+                    log.Info("GDM driver was sent a " + pom + " command");
+                    curWavenumber+=0.5;
+                    while (stopWatch.ElapsedMilliseconds < single_step_duration) { 
+                    
+                    };
+                    MotorPort.Write("0");
+                    stepNumber++;
+                    stopWatch.Stop();
+                    log.Debug("Step took " + stopWatch.ElapsedMilliseconds+ " ms");
+
+                }
+            }
+        }
+        public void SetPortName(string pname) { MotorPort.PortName = pname; log.Info("GDM Port name was set to :"+pname); }
+        public void Initialize() { MotorPort.Open(); bInitialized = true; }
+        public void ShutDown() { MotorPort.Close(); bInitialized = false; }
+        public double GetCurrentWN() { return curWavenumber; }
+    }
+    
+    }
+
---
+++ boxCarLib/Boxcar.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+using System.Diagnostics;
+
+namespace boxCarLib
+{
+    
+    public class Boxcar: IDisposable
+    {
+        const long  boxcar_timeout= 5000;
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private readonly SerialPort boxCarPort;
+        private string readBuffer;
+        bool bresponseReceived,bTimedOut;
+        public Boxcar(string pName) {
+            boxCarPort = new SerialPort
+            {
+                PortName = pName,
+                BaudRate = 19200,
+                Parity = Parity.None,
+                StopBits = StopBits.Two,
+                NewLine = "\r"
+            };
+            boxCarPort.DataReceived += BoxCarPort_DataReceived;
+        }
+        public Boxcar()
+        {
+            boxCarPort = new SerialPort
+            {
+                PortName = "C",
+                BaudRate = 19200,
+                Parity = Parity.None,
+                StopBits = StopBits.Two,
+                NewLine = "\r"
+            };
+            boxCarPort.DataReceived += BoxCarPort_DataReceived;
+        }
+        public string LastResp() { return readBuffer; }
+        
+        public string GetValue(int channel)
+        {
+            log.Info("Request sent");
+            string com = "?" + channel.ToString();
+            SendCommand(com);
+            Stopwatch swResp = new Stopwatch();
+            swResp.Start();
+            while ((!bresponseReceived)&&(swResp.ElapsedMilliseconds<boxcar_timeout) ) { Thread.Sleep(10); };
+            if (!bresponseReceived)
+            {
+                log.Error("Communication timed out");
+                bTimedOut = true;
+            }
+            log.Warn("End of listening loop");
+            return LastResp();
+        }
+
+        private void BoxCarPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            GetResponse(out readBuffer);
+
+        }
+        public void SetPortName(string pname) { boxCarPort.PortName = pname; }
+        public void SendCommand(string command)
+        {
+            if (boxCarPort.IsOpen)
+            {
+                boxCarPort.Write(command + "\r");
+                log.Debug(command + " was sent to boxcar");
+                bresponseReceived = false;
+                bTimedOut = false;
+            }
+        }
+        private void GetResponse(out string response)
+        {
+            
+            if (boxCarPort.IsOpen)
+            {
+                response = boxCarPort.ReadLine();
+                log.Info("Response obtained was "+response);
+                bresponseReceived = true;
+                log.Info("Flag set");
+            }
+            else response = "Failure";
+        }
+        public void Parse(string com){
+
+            log.Info("Boxcar command "+com +" sent");
+            SendCommand(com);
+        }
+        public void Initialize() {
+            boxCarPort.Open();
+            log.Debug("trying to send w0 ");
+            SendCommand("W0");
+            log.Info("The W0 command was sent, boxcar ready for further instructions");
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // dispose managed resources
+                boxCarPort.Dispose();
+            }
+            // free native resources
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
